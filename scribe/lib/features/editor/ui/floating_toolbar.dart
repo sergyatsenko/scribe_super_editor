d1d915f0b8ffe228dab8672d8bf323264ea686d2
Sergey Yatsenko <syatsenko@gmail.com>
2025-05-23 15:22:44 -0600

    context menu

--- scribe/lib/features/editor/ui/floating_toolbar.dart
+++ scribe/lib/features/editor/ui/floating_toolbar.dart
@@ -582,100 +582,116 @@ class _EditorToolbarState extends State<EditorToolbar> {
         child: SizedBox(
           height: 40,
           // Enable horizontal scrolling for the toolbar items.
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            // Use a Row to layout the toolbar items horizontally.
-            // mainAxisSize.min ensures the Row takes up only necessary space if content is smaller than max width.
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // Only allow the user to select a new type of text node if
-                // the currently selected node can be converted.
-                if (_isConvertibleNode()) ...[
-                  Tooltip(
-                    message: AppLocalizations.of(context)!.labelTextBlockType,
-                    child: _buildBlockTypeSelector(),
-                  ),
-                  _buildVerticalDivider(),
+          child: ShaderMask(
+            shaderCallback: (Rect bounds) {
+              return LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: <Color>[
+                  Colors.transparent,
+                  Colors.black,
+                  Colors.black,
+                  Colors.transparent
                 ],
-                Center(
-                  child: IconButton(
-                    onPressed: _toggleBold,
-                    icon: const Icon(Icons.format_bold),
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelBold,
+                stops: [0.0, 0.05, 0.95, 1.0],
+              ).createShader(bounds);
+            },
+            blendMode: BlendMode.dstIn,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              // Use a Row to layout the toolbar items horizontally.
+              // mainAxisSize.min ensures the Row takes up only necessary space if content is smaller than max width.
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Only allow the user to select a new type of text node if
+                  // the currently selected node can be converted.
+                  if (_isConvertibleNode()) ...[
+                    Tooltip(
+                      message: AppLocalizations.of(context)!.labelTextBlockType,
+                      child: _buildBlockTypeSelector(),
+                    ),
+                    _buildVerticalDivider(),
+                  ],
+                  Center(
+                    child: IconButton(
+                      onPressed: _toggleBold,
+                      icon: const Icon(Icons.format_bold),
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelBold,
+                    ),
                   ),
-                ),
-                Center(
-                  child: IconButton(
-                    onPressed: _toggleItalics,
-                    icon: const Icon(Icons.format_italic),
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelItalics,
+                  Center(
+                    child: IconButton(
+                      onPressed: _toggleItalics,
+                      icon: const Icon(Icons.format_italic),
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelItalics,
+                    ),
                   ),
-                ),
-                Center(
-                  child: IconButton(
-                    onPressed: _toggleStrikethrough,
-                    icon: const Icon(Icons.strikethrough_s),
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelStrikethrough,
+                  Center(
+                    child: IconButton(
+                      onPressed: _toggleStrikethrough,
+                      icon: const Icon(Icons.strikethrough_s),
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelStrikethrough,
+                    ),
                   ),
-                ),
-                Center(
-                  child: IconButton(
-                    onPressed: _toggleSuperscript,
-                    icon: const Icon(Icons.superscript),
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelSuperscript,
+                  Center(
+                    child: IconButton(
+                      onPressed: _toggleSuperscript,
+                      icon: const Icon(Icons.superscript),
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelSuperscript,
+                    ),
                   ),
-                ),
-                Center(
-                  child: IconButton(
-                    onPressed: _toggleSubscript,
-                    icon: const Icon(Icons.subscript),
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelSubscript,
+                  Center(
+                    child: IconButton(
+                      onPressed: _toggleSubscript,
+                      icon: const Icon(Icons.subscript),
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelSubscript,
+                    ),
                   ),
-                ),
-                Center(
-                  child: IconButton(
-                    onPressed:
-                        _areMultipleLinksSelected() ? null : _onLinkPressed,
-                    icon: const Icon(Icons.link),
-                    color: _isSingleLinkSelected()
-                        ? const Color(0xFF007AFF)
-                        : IconTheme.of(context).color,
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelLink,
+                  Center(
+                    child: IconButton(
+                      onPressed:
+                          _areMultipleLinksSelected() ? null : _onLinkPressed,
+                      icon: const Icon(Icons.link),
+                      color: _isSingleLinkSelected()
+                          ? const Color(0xFF007AFF)
+                          : IconTheme.of(context).color,
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelLink,
+                    ),
                   ),
-                ),
-                // Only display alignment controls if the currently selected text
-                // node respects alignment. List items, for example, do not.
-                if (_isTextAlignable()) //
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      _buildVerticalDivider(),
-                      Tooltip(
-                        message:
-                            AppLocalizations.of(context)!.labelTextAlignment,
-                        child: _buildAlignmentSelector(),
-                      ),
-                    ],
-                  ),
+                  // Only display alignment controls if the currently selected text
+                  // node respects alignment. List items, for example, do not.
+                  if (_isTextAlignable()) //
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        _buildVerticalDivider(),
+                        Tooltip(
+                          message:
+                              AppLocalizations.of(context)!.labelTextAlignment,
+                          child: _buildAlignmentSelector(),
+                        ),
+                      ],
+                    ),
 
-                _buildVerticalDivider(),
-                Center(
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.more_vert),
-                    splashRadius: 16,
-                    tooltip: AppLocalizations.of(context)!.labelMoreOptions,
+                  _buildVerticalDivider(),
+                  Center(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.more_vert),
+                      splashRadius: 16,
+                      tooltip: AppLocalizations.of(context)!.labelMoreOptions,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
